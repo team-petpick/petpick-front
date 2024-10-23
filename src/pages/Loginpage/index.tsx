a7034cca581f4b3433d5c6fcca0c70e2c2aac925
hyorish03 <hyorish2001@gmail.com>
2024-10-23 15:29:03 +0900

    feat(ZH-80): 회원가입과 로그인을 구분해 안내 문구 변경되도록 구현

--- src/pages/Loginpage/index.tsx
+++ src/pages/Loginpage/index.tsx
@@ -2,6 +2,9 @@ import { GoogleLogin, PetpickLogo } from '@assets/svg';
 import * as S from './index.style';
 
 const LoginPage = () => {
+  const urlPath = window.location.pathname;
+  const parsedString = urlPath.split('/').pop();
+  const pathString = parsedString === 'login' ? '로그인' : '회원가입';
   return (
     <S.LoginLayout>
       <PetpickLogo width={200} height={200} />{' '}
@@ -13,12 +16,12 @@ const LoginPage = () => {
           marginTop: '100px',
         }}
       >
-        <S.SignUpText>회원가입</S.SignUpText>
+        {<S.SignUpText>{pathString}</S.SignUpText>}
         <S.SignUpExplainText>
-          소셜 로그인을 사용하면 간편하게 <br /> 회원가입이 가능합니다.
+          소셜 로그인을 사용하면 간편하게 <br /> {pathString}이 가능합니다.
         </S.SignUpExplainText>
         <S.SignUpAnnounce>
-          <S.Emphasize>3초만에 </S.Emphasize>빠른 회원가입
+          <S.Emphasize>3초만에 </S.Emphasize>빠른 {pathString}
         </S.SignUpAnnounce>
         <S.ArrowBox />
         <GoogleLogin />
