f08fc4e845450b2cc909a4ce2f7ee56768e9ada8
hyorish03 <hyorish2001@gmail.com>
2024-10-23 13:31:07 +0900

    feat(-): 폴더 충돌 해결

---
+++ src/pages/mainpage/index.tsx
@@ -0,0 +1,13 @@
+import Layout from '@layout/Layout';
+import Category from './components/Category';
+
+const MainPage = () => {
+  return (
+    <Layout>
+      <Category />
+      <h1>Main Page</h1>
+    </Layout>
+  );
+};
+
+export default MainPage;
