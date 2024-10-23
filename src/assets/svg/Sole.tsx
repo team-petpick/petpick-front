442e993231d296b83fa5e3b2ea08b69557aa918a
rhdmswls12 <rhgusdnr1293@naver.com>
2024-10-23 15:56:38 +0900

    feat(ZH-74): 카테고리 UI에 사용할 svg 추가

---
+++ src/assets/svg/Sole.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgSole = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 17" {...props}>
+    <path fill="#101010" d="M.16 16.12h16v-16h-16z" />
+  </svg>
+);
+export default SvgSole;
--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,8 +1,12 @@
 export { default as Bell } from './Bell';
 export { default as Cart } from './Cart';
+export { default as Cat } from './Cat';
+export { default as Dog } from './Dog';
 export { default as Like } from './Like';
 export { default as Menubar } from './Menubar';
 export { default as Minus } from './Minus';
 export { default as Plus } from './Plus';
 export { default as RightArrow } from './RightArrow';
 export { default as Search } from './Search';
+export { default as Sole } from './Sole';
+export { default as Vite } from './Vite';
