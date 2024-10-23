7b1bee47e6eac8117fc1b54bc1fabaddd1d4f63f
kyoul10121 <kyoul10121@naver.com>
2024-10-23 16:37:13 +0900

    feat(ZH-70): 비회원을 위한 로그인 전역 상태관리 zustand 사용해서 구현

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,10 +1,28 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface IAuthStore {
-  isLogin: boolean;
-  setIsLogin: (isLogin: boolean) => void;
+  isLoggedIn: boolean;
+  login: () => void;
+  logout: () => void;
 }
-export const useAuthStore = create<IAuthStore | null>((set) => ({
-  isLogin: false,
-  setIsLogin: (isLogin: boolean) => set({ isLogin }),
-}));
+export const useAuthStore = create(
+  persist<IAuthStore>(
+    (set) => ({
+      isLoggedIn: false,
+      login: () => {
+        const userLocalStorage = localStorage.getItem('accessToken');
+        if (userLocalStorage) {
+          set({ isLoggedIn: true });
+        }
+      },
+      logout: () => {
+        set({ isLoggedIn: false });
+        localStorage.clear();
+      },
+    }),
+    {
+      name: 'userLocalStorage',
+    },
+  ),
+);
