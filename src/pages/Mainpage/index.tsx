441d8caabad1ded492d949ce87edb5dc0d223e24
kyoul10121 <kyoul10121@naver.com>
2024-10-25 02:09:45 +0900

    feat(ZH-76): 메인페이지 필터(인기순, 판매순) 컴포넌트 구현

--- src/pages/Mainpage/index.tsx
+++ src/pages/Mainpage/index.tsx
@@ -1,10 +1,12 @@
 import Layout from '@layout/Layout';
 import Category from './components/Category';
+import ProductFilter from './components/ProductFilter';
 
 const MainPage = () => {
   return (
     <Layout>
       <Category />
+      <ProductFilter />
       <h1>Main Page</h1>
     </Layout>
   );
