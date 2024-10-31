56341f5cffefb9549122bc7bd1490ea659eb7e5f
rhdmswls12 <rhgusdnr1293@naver.com>
2024-10-31 16:09:14 +0900

    fix(ZH-81): 새로고침 시에도 사용자 이름 유지하도록 수정

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,12 +1,20 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface IUserStore {
   userName: string | null;
   setUserName: (name: string) => void;
   clearUserName: () => void;
 }
-export const useUserStore = create<IUserStore>((set) => ({
-  userName: '',
-  setUserName: (name) => set({ userName: name }),
-  clearUserName: () => set({ userName: null }),
-}));
+export const useUserStore = create(
+  persist<IUserStore>(
+    (set) => ({
+      userName: null,
+      setUserName: (name) => set({ userName: name }),
+      clearUserName: () => set({ userName: null }),
+    }),
+    {
+      name: 'userStore',
+    },
+  ),
+);
