2608fc1d55db98302a5095f7ffdb70122a196480
hyorish03 <hyorish2001@gmail.com>
2024-10-31 16:49:29 +0900

    feat(ZH-102): 펫 등록 페이지 공통 버튼 개발

---
+++ src/pages/RegisterMyPet/components/button/index.tsx
@@ -0,0 +1,15 @@
+import { IButtonStatus } from '@pages/RegisterMyPet/types';
+import * as S from '../../styles/button.styles';
+
+interface IButtonProps extends IButtonStatus {
+  buttonName: string;
+  onClick?: () => void;
+}
+const Button = ({ onClick, buttonDirection, isActive, buttonName }: IButtonProps) => {
+  return (
+    <S.ButtonWrapper onClick={onClick} buttonDirection={buttonDirection} isActive={isActive}>
+      {buttonName}
+    </S.ButtonWrapper>
+  );
+};
+export default Button;
--- src/pages/RegisterMyPet/index.tsx
+++ src/pages/RegisterMyPet/index.tsx
@@ -1,5 +1,16 @@
+import Layout from '@components/layouts/Layout';
+import Button from './components/button';
+
 const RegisterMyPet = () => {
-  return <div></div>;
+  return (
+    <Layout footerVisible={false}>
+      <div>RegisterMyPet</div>
+      <Button buttonDirection={'next'} isActive={true} buttonName={'다음 활성화'} />
+      <Button buttonDirection={'next'} isActive={false} buttonName={'다음 비활성화'} />
+      <Button buttonDirection={'prev'} isActive={false} buttonName={'이전'} />
+      <Button buttonDirection={'prev'} isActive={true} buttonName={'이전'} />
+    </Layout>
+  );
 };
 
 export default RegisterMyPet;
