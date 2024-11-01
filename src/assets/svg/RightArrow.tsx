7c6a7dab25ed03147eddd09ae7e9301c9a34dfa1
hyorish03 <hyorish2001@gmail.com>
2024-11-01 15:06:22 +0900

    fix(-): 사용하지 않는 변수 삭제

--- src/assets/svg/RightArrow.tsx
+++ src/assets/svg/RightArrow.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgRightArrow = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" {...props}>
