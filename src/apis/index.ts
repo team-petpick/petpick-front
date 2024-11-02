70b712809ed74daaebba1534a2418e12b78fabbb
rhdmswls12 <rhgusdnr1293@naver.com>
2024-11-02 18:52:24 +0900

    fix(ZH-81): access token 명 수정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-
 const instance = axios.create({
   baseURL: 'https://back.petpick.store',
   headers: {
@@ -7,19 +6,16 @@ const instance = axios.create({
   },
   withCredentials: true,
 });
-
 instance.interceptors.request.use(
   (config) => {
     const accessToken = localStorage.getItem('accessToken');
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
-
     return config;
   },
   (error) => Promise.reject(error),
 );
-
 instance.interceptors.response.use(
   (response) => {
     console.log('response>>>', response);
@@ -28,45 +24,37 @@ instance.interceptors.response.use(
   async (error) => {
     console.log('error', error.config);
     const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-
-      try {
-        const refreshResponse = await instance.post(
-          '/api/v1/auth/token',
-          {},
-          {
-            headers: {
-              'Content-Type': 'application/json',
+    if (error.response) {
+      if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        try {
+          const refreshResponse = await instance.post(
+            '/api/v1/auth/token',
+            {},
+            {
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              withCredentials: true,
             },
-            withCredentials: true,
-          },
-        );
-        console.log('REFRESH>>>>>>>', refreshResponse);
-        const newAccessToken = refreshResponse.data.access_token;
-        localStorage.setItem('accessToken', newAccessToken);
+          );
 
-        instance.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
-        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-
-        return instance(originalRequest);
-      } catch (err) {
-        console.error('Token refresh failed:', err);
-        localStorage.removeItem('accessToken');
-        return Promise.reject(err);
+          const newAccessToken = refreshResponse.data.accessToken;
+          localStorage.setItem('accessToken', newAccessToken);
+          instance.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
+          originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+          return instance(originalRequest);
+        } catch (err) {
+          console.error('Token refresh failed:', err);
+          localStorage.removeItem('accessToken');
+          return Promise.reject(err);
+        }
       }
+    } else {
+      // Handle case where error.response is undefined
+      console.error('Error response is undefined', error);
     }
     return Promise.reject(error);
   },
 );
-// instance.interceptors.response.use(
-//   (res) => res,
-//   async (error) => {
-//     const {config} = error;
-//     if (error.response?.status === 401) {
-//       const res = await instance.post('/api/v1/auth/token');
-//       localStorage.setItem('accessToken', res.data.access_token);
-//     }
-//   }
-// )
 export default instance;
