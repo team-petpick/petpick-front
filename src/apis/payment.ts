8bbef3ed5792a12a2fb04275343929bf02de5993
kyoul10121 <kyoul10121@naver.com>
2024-11-05 14:57:59 +0900

    feat(ZH-129): 더미데이터 추가, orderId를 orderSerialCode로 변경 후 백으로 전송

--- src/apis/payment.ts
+++ src/apis/payment.ts
@@ -1,46 +1,61 @@
-import axios, { AxiosError } from 'axios';
+import { AxiosError } from 'axios';
 import instance from './instance';
 
 export const sendPaymentSuccess = async (
-  orderId: string,
   paymentKey: string,
   amount: number,
   userId: number, // 추가된 필드
+  orderSerialCode: string,
   orderRequest: string,
+  orderDetails: Array<{ productId: number; orderDetailPrice: number; orderDetailCnt: number }>,
+  address: {
+    addressId: number | null;
+    userId: number;
+    addressName: string;
+    addressZipcode: number;
+    addressAddr: string;
+    addressAddrDetail: string;
+    addressTel: string;
+    addressRequest: string;
+    addressDefault: string;
+  },
 ) => {
   try {
     // 유효성 검사 및 데이터 확인 로그 추가
     console.log('전송할 데이터:', {
-      orderId,
+      orderSerialCode,
       paymentKey,
       amount,
       userId,
       orderRequest,
+      orderDetails,
+      address,
     });
 
     const requestData = {
-      orderId,
+      orderSerialCode,
       paymentKey,
       amount,
       userId,
       orderRequest,
+      orderDetails,
+      address,
     };
-    // const response = await instance.post(`/payment/success`, requestData);
-    const response = axios
-      .get(
-        `/payment/success?orderid=${orderId}&paymentKey=${paymentKey}&amount=${amount}&user_id=${userId}&order_request=${orderRequest}`,
-      )
-      .then((res) => res.status);
-
+    const response = await instance.post(`/payment/success`, requestData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    console.log('response -> ', response);
     return response;
   } catch (error) {
     console.log(error);
 
-    //   if (error instanceof AxiosError && error.response) {
-    //     console.error('서버 에러 메시지:', error.response.data); // 서버에서 반환한 에러 메시지
-    //   } else {
-    //     console.error('요청 자체에 실패하거나 네트워크 오류가 발생했습니다:', error);
-    //   }
-    //   throw error;
+    if (error instanceof AxiosError && error.response) {
+      console.error('서버 에러 메시지:', error.response.data); // 서버에서 반환한 에러 메시지
+    } else {
+      console.error('요청 자체에 실패하거나 네트워크 오류가 발생했습니다:', error);
+    }
+    throw error;
   }
 };
--- src/pages/TossPaymentPage/SucessPage.tsx
+++ src/pages/TossPaymentPage/SucessPage.tsx
@@ -6,7 +6,7 @@ export default function SuccessPage() {
   const [searchParams] = useSearchParams();
 
   // URL에서 쿼리 파라미터로 받은 값 추출
-  const orderId = searchParams.get('orderId');
+  const orderSerialCode = searchParams.get('orderId');
   const paymentKey = searchParams.get('paymentKey');
   const amount = searchParams.get('amount');
 
@@ -14,18 +14,44 @@ export default function SuccessPage() {
 
   const userId = 1;
   const orderRequest = '빨리 주이소 !!';
+  //더미데이터 추가됨
+  const orderDetails = [
+    {
+      productId: 1,
+      orderDetailPrice: 20000,
+      orderDetailCnt: 1,
+    },
+    {
+      productId: 2,
+      orderDetailPrice: 30000,
+      orderDetailCnt: 1,
+    },
+  ];
+  const address = {
+    addressId: null,
+    userId: 1,
+    addressName: '집',
+    addressZipcode: 10233,
+    addressAddr: '강남구 신림동',
+    addressAddrDetail: '오송빌딩',
+    addressTel: '010-1234-5678',
+    addressRequest: '문앞ㅇ[ 두고 가세[요',
+    addressDefault: 'DEFAULT',
+  };
   useEffect(() => {
     async function handlePaymentSuccess() {
-      if (orderId && paymentKey && amount) {
+      if (orderSerialCode && paymentKey && amount) {
         try {
           const response = await sendPaymentSuccess(
-            orderId,
             paymentKey,
             Number(amount),
             userId,
             orderRequest,
+            orderSerialCode,
+            orderDetails,
+            address,
           );
-          if (response === 200) {
+          if (response.status === 200) {
             console.log('결제 정보가 백엔드에 성공적으로 전송되었습니다.');
           }
         } catch (error) {
@@ -36,7 +62,7 @@ export default function SuccessPage() {
 
     // 모든 파라미터가 존재할 경우에만 호출
     handlePaymentSuccess();
-  }, [orderId, paymentKey, amount]);
+  }, [orderSerialCode, paymentKey, amount]);
 
   return <h2>결제가 성공적으로 완료되었습니다!</h2>;
 }
