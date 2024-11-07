caf0230f85ddb9e3906b51be8ca37525ebc4faef
kyoul10121 <kyoul10121@naver.com>
2024-11-07 22:46:56 +0900

    feat(ZH-138): 불필요한 코드 삭제

--- src/pages/Shoppingcartpage/components/ShoppingAddress.tsx
+++ src/pages/Shoppingcartpage/components/ShoppingAddress.tsx
@@ -31,7 +31,6 @@ const ShoppingAddress = () => {
       detailAddress: ` ${newDetailAddress}`,
       zipCode: `${newZipCode}`,
     });
-    // setUserAddress(`${newBaseAddress} ${newDetailAddress} ${newZipCode}`);
   }, [location.state, setUserAddress]);
 
   return (
