a8e0268e6b145fc67902ce51d362aaba3f65d1e9
hyorish03 <hyorish2001@gmail.com>
2024-11-08 11:50:41 +0900

    fix(-): 소수점 문제 해결

--- src/utils/addCommaToPrice.ts
+++ src/utils/addCommaToPrice.ts
@@ -2,5 +2,7 @@ export const addCommaToPrice = (price: number | undefined) => {
   if (price === undefined || price === null) {
     return '0'; // 기본값을 설정
   }
-  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return Math.floor(price)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
